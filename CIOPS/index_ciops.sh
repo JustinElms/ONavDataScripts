6a67729ad0df69b3741ee72f2c65aaa9212ceb9b
JustinElms <justin.elms@dfo-mpo.gc.ca>
2024-01-25 19:21:00 +0000

    Fix script formatting

--- CIOPS/index_ciops.sh
+++ CIOPS/index_ciops.sh
@@ -46,7 +46,7 @@ rm -r /data/hpfx.collab.science.gc.ca/$(date -d "-1 years" +%Y%m%d)
 
 ssh ubuntu@u2004-index "cd index-scripts ; ./ciops.sh ${RUN}"
 
-# # replace production dataset db
+# replace production dataset db
 
 lxc file pull u2004-index/home/ubuntu/db/ciops-east_fc_2dll.sqlite3 /data/db/
 lxc file pull u2004-index/home/ubuntu/db/ciops-east_fc_3dll.sqlite3 /data/db/
