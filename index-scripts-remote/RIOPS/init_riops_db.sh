05a14ceefa3ff6c1b8d63d0323d623edfe737087
JustinElms <justin.elms@dfo-mpo.gc.ca>
2024-03-12 00:32:05 +0000

    break up large archive indexing

--- index-scripts-remote/RIOPS/init_riops_db.sh
+++ index-scripts-remote/RIOPS/init_riops_db.sh
@@ -7,8 +7,16 @@ cd ${HOME}/netcdf-timestamp-mapper
 rm ${HOME}/db/riops-fc2dps-archive.sqlite3
 rm ${HOME}/db/riops-fc3dps-archive.sqlite3
 
-find /data/hpfx.collab.science.gc.ca/*/WXO-DD/model_riops/netcdf/forecast/polar_stereographic/2d/ -type f > riops-fc2dps-archive.txt
-${HOME}/netcdf-timestamp-mapper/build/nc-timestamp-mapper -n riops-fc2dps-archive -i /data/hpfx.collab.science.gc.ca/$(date +%Y%m%d)/WXO-DD/model_riops/ -o ${HOME}/db --file-list riops-fc2dps-archive.txt -h
+DATE=$(date -d "-1 years" +%Y%m)
+ENDDATE=$(date +%Y%m)
 
-find /data/hpfx.collab.science.gc.ca/*/WXO-DD/model_riops/netcdf/forecast/polar_stereographic/3d/ -type f > riops-fc3dps-archive.txt
-${HOME}/netcdf-timestamp-mapper/build/nc-timestamp-mapper -n riops-fc3dps-archive -i /data/hpfx.collab.science.gc.ca/$(date +%Y%m%d)/WXO-DD/model_riops/ -o ${HOME}/db --file-list riops-fc3dps-archive.txt -h
+while [ $DATE -le $ENDDATE ]; do 
+
+    find /data/hpfx.collab.science.gc.ca/${DATE}*/WXO-DD/model_riops/netcdf/forecast/polar_stereographic/2d/ -type f > riops-fc2dps-archive.txt
+    ${HOME}/netcdf-timestamp-mapper/build/nc-timestamp-mapper -n riops-fc2dps-archive -i /data/hpfx.collab.science.gc.ca/$(date +%Y%m%d)/WXO-DD/model_riops/ -o ${HOME}/db --file-list riops-fc2dps-archive.txt -h
+
+    find /data/hpfx.collab.science.gc.ca/${DATE}*/WXO-DD/model_riops/netcdf/forecast/polar_stereographic/3d/ -type f > riops-fc3dps-archive.txt
+    ${HOME}/netcdf-timestamp-mapper/build/nc-timestamp-mapper -n riops-fc3dps-archive -i /data/hpfx.collab.science.gc.ca/$(date +%Y%m%d)/WXO-DD/model_riops/ -o ${HOME}/db --file-list riops-fc3dps-archive.txt -h
+
+    DATE=$(date -d "${DATE}01 + 1 month" +%Y%m)
+done
