2a690ffeb1fd056f1231387dcc6b8c7d388c2190
JustinElms <justinwelms@gmail.com>
2026-02-11 13:21:31 +0000

    update 2d/3d caps filter

--- index-scripts-remote/CAPS/init_caps_db.sh
+++ index-scripts-remote/CAPS/init_caps_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-find /data/hpfx.collab.science.gc.ca/*/WXO-DD/model_caps/3km/ -type f | grep -E "(Sfc|0.5m)" > ${HOME}/db/caps-fc2drp.txt
+find /data/hpfx.collab.science.gc.ca/*/WXO-DD/model_caps/3km/ -name \*Sfc\*.nc -type f > ${HOME}/db/caps-fc2drp.txt
 
 # Indexes historical datasets that will be exanded upon by new data
 
@@ -11,10 +11,10 @@ DATE=$(date -d "-2 years" +%Y%m)
 ENDDATE=$(date +%Y%m)
 
 while [ $DATE -le $ENDDATE ]; do
-    find /data/hpfx.collab.science.gc.ca/${DATE}*/WXO-DD/model_caps/3km/ -type f | grep -E "(Sfc|0.5m)" > ${HOME}/db/caps-fc2drp.txt
+    find /data/hpfx.collab.science.gc.ca/${DATE}*/WXO-DD/model_caps/3km/ -name \*Sfc\*.nc -type f > ${HOME}/db/caps-fc2drp.txt
     ${HOME}/netcdf-timestamp-mapper/build/nc-timestamp-mapper -n caps-fc2drp-archive -i /data/hpfx.collab.science.gc.ca/ -o ${HOME}/db --file-list ${HOME}/db/caps-fc2drp.txt -h
 
-    find /data/hpfx.collab.science.gc.ca/${DATE}*/WXO-DD/model_caps/3km/ -type f | grep -E "(-all_)" > ${HOME}/db/caps-fc3drp.txt
+    find /data/hpfx.collab.science.gc.ca/${DATE}*/WXO-DD/model_caps/3km/ -name \*all\*.nc -type f > ${HOME}/db/caps-fc3drp.txt
     ${HOME}/netcdf-timestamp-mapper/build/nc-timestamp-mapper -n caps-fc3drp-archive -i /data/hpfx.collab.science.gc.ca/ -o ${HOME}/db --file-list ${HOME}/db/caps-fc3drp.txt -h
 
     DATE=$(date -d "${DATE}01 + 1 month" +%Y%m)
