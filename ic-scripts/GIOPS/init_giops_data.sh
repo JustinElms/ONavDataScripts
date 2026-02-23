cc49dc1f2812184cabcc58c53b6111bb900a68de
JustinElms <justinwelms@gmail.com>
2026-02-23 15:29:46 +0000

    fix giops indexing

--- ic-scripts/GIOPS/init_giops_data.sh
+++ ic-scripts/GIOPS/init_giops_data.sh
@@ -27,4 +27,4 @@ while (( $(date -d "${DATE}" +%s) < $(date -d "${END_DATE}" +%s) )); do
 done
 
 ssh ubuntu@u2204-icechunk "cd icechunk/ ; source env/icechunk-env.sh ; python ic_interface/add_nc_data.py giops_day -s"
-ssh ubuntu@u2204-icechunk "cd icechunk/ ; source env/icechunk-env.sh ; python ic_interface/add_nc_data.py gdwps_2dll -s"
+ssh ubuntu@u2204-icechunk "cd icechunk/ ; source env/icechunk-env.sh ; python ic_interface/add_nc_data.py giops_fc_10d_2dll -s"
